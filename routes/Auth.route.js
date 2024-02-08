e3a28eed1a26a3bd1755f652e7d3bb8e71b4e52c
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-02-08 20:28:19 +0530

    Add: Auth Controller & Auth Route

--- routes/Auth.route.js
+++ routes/Auth.route.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
-
+const { registerController, loginController,
+    logoutController, refetchUserController } = require("../controllers/authController")
 
 //REGISTER
 router.post("/register",registerController)
